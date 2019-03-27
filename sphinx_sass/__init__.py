ceddbc606446051fc2da9625a28a83d1808259bc
Mark Wibrow <m.wibrow@gmail.com>
2019-03-27 09:38:47 +0000

    Source maps must be embedded to work

--- sphinx_sass/__init__.py
+++ sphinx_sass/__init__.py
@@ -35,65 +35,35 @@ def compile_sass(entry, output, compile_options=None, sass_vars=None):
 
     entry = str(entry)
     output = str(output)
-
-    if sass_vars:
-        _sass = '\n'.join(['${}:{};'.format(var, val)
-                           for var, val in sass_vars.items()])
-        _sass += '\n@import"{}";'.format(os.path.abspath(entry))
-        with tempfile.TemporaryDirectory() as tmpdir:
-            _entry = os.path.join(tmpdir, '_entry.scss')
-            with open(_entry, 'w') as file_out:
-                file_out.write(_sass)
-            _compile_sass(_entry, output, compile_options)
-
-    else:
-        _compile_sass(entry, output, compile_options)
-
-
-def _compile_sass(entry, output, compile_options):
-    """Wrapper around libsaass."""
-
     compile_options = compile_options or {}
-    compile_options.pop('filename', None)
-    compile_options.pop('dirname', None)
-    compile_options.pop('string', None)
 
-    # Not needed.
+    for option in ['filename', 'string', 'filename', 'source_map_filename']:
+        compile_options.pop(option, None)
+
     include_paths = [os.path.dirname(entry)]
     include_paths.extend(compile_options.pop('include_paths', []))
+    compile_options['include_paths'] = include_paths
 
-    source_map_filename = compile_options.get('source_maps_filename', None)
-    source_maps = compile_options.pop(
-        'source_maps', None) or source_map_filename
-    source_map_output = None
-    if source_maps:
-        if not source_map_filename:
-            components = output.split(os.path.extsep)
-            components.insert(-1, 'map')
-            source_map_output = os.path.extsep.join(components)
-            source_map_filename = os.path.basename()
-            compile_options['source_map_filename'] = source_map_output
+    header = ''
+    if sass_vars:
+        header = '\n'.join(['${}:{};'.format(var, val)
+                            for var, val in sass_vars.items()])
+        header += '\n@import"{}";\n'.format(os.path.abspath(entry))
 
-    os.makedirs(os.path.dirname(output), exist_ok=True)
+    with open(entry, 'r') as file_in:
+        source = file_in.read()
 
-    if source_maps:
-        css, source_map = sass.compile(
-            filename=entry,
-            include_paths=include_paths,
-            **compile_options)
-    else:
+    css = ''
+    if source.strip():
+        source = header + source
         css = sass.compile(
-            filename=entry,
-            include_paths=include_paths,
+            string=source,
             **compile_options)
-        source_map = ''
 
     if css.strip():
+        os.makedirs(os.path.dirname(output), exist_ok=True)
         with open(output, 'w') as file_out:
             file_out.write(css)
-    if source_map.strip() and source_map_output:
-        with open(source_map_output, 'w') as file_out:
-            file_out.write(source_map)
 
 
 def init(app):
